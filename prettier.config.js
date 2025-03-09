edc3390ed7e5ebf9498aea79ab1f825bbfdb3107
Ruggery Iury Corrêa <ruggeryiury@gmail.com>
2025-03-09 16:39:08 -0300

    Update project configurations

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('prettier').Config} */
-const prettierConfig = {
-  trailingComma: 'es5',
-  semi: false,
-  singleQuote: true,
-  printWidth: 1000,
-}
-
-export default prettierConfig
