3f6165801afaf62dc06c901d521b66c91411bd67
Ruggery Iury Corrêa <ruggeryiury@gmail.com>
2025-03-14 20:07:48 -0300

    Update to v1.2.0, Reverting building for both CommonJS and ESModules again

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,30 @@
+import { type RequiredDeep } from 'type-fest'
+
+/**
+ * Iterates the provided `object` to append the values from this key, and return the files appended.
+ * - - - -
+ * @param {Record<string, any>} object The object with values to be iterable and appended to the return object.
+ * @returns {Record<string, any>}
+ */
+export const iterateObjKeysAndAppendValues = (object: Record<string, any>): Record<string, any> => {
+  const newO: Record<string, any> = {}
+
+  for (const key of Object.keys(object)) {
+    if (typeof object[key] === 'object') newO[key] = { ...object[key], ...iterateObjKeysAndAppendValues(object[key]) }
+    else newO[key] = object[key]
+  }
+
+  return newO
+}
+
 /**
  * Utility function to merge default options with user-defined ones.
  * - - - -
- * @param {Required<NonNullable<T>>} defaultOptions - The default options of the function.
- * @param {Partial<T> | undefined} userOptions - `OPTIONAL` User-provided options with properties to override any default option property. If `undefined` or an empty object, no default properties will be merged.
- * @returns {Required<NonNullable<T>>} - The default options merged with given user options.
+ * @param {RequiredDeep<T>} defaultOptions The default options of the function.
+ * @param {Partial<T> | undefined} userOptions `OPTIONAL` User-provided options with properties to override any default option property. If `undefined` or an empty object, no default properties will be merged.
+ * @returns {RequiredDeep<T>} The default options merged with given user options.
  */
-const setDefaultOptions = <T>(defaultOptions: Required<NonNullable<T>>, userOptions?: Partial<T>): Required<NonNullable<T>> => {
+export const setDefaultOptions = <T>(defaultOptions: RequiredDeep<T>, userOptions?: Partial<T>): RequiredDeep<T> => {
   if (!userOptions) return defaultOptions
-
-  const loopObjVariables = (o: Record<string, any>): Record<string, any> => {
-    const newO: Record<string, any> = {}
-    for (const key of Object.keys(o)) {
-      if (typeof o[key] === 'object') newO[key] = loopObjVariables(o[key])
-      else newO[key] = o[key]
-    }
-
-    return newO
-  }
-
-  return { ...defaultOptions, ...loopObjVariables(userOptions) }
+  return { ...(defaultOptions as Record<string, any>), ...iterateObjKeysAndAppendValues(userOptions) } as RequiredDeep<T>
 }
-
-export default setDefaultOptions
--- tsup.config.js
+++ tsup.config.js
@@ -2,20 +2,19 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['esm'],
+  format: ['esm', 'cjs'],
   splitting: false,
   clean: true,
   dts: true,
-  target: 'es2020',
-  platform: 'browser',
+  target: 'es6',
+  platform: 'neutral',
   outDir: 'dist',
   bundle: false,
   treeshake: true,
   tsconfig: './prod.tsconfig.json',
-  esbuildOptions(options) {
-    options.banner = {
-      js: '"use strict";',
-    }
+  sourcemap: true,
+  esbuildOptions: (options) => {
+    options.banner = { js: '"use strict";' }
   },
-  outExtension: () => ({ js: '' }),
+  outExtension: ({ format }) => ({ js: format === 'cjs' ? '.cjs' : '.js' }),
 })
