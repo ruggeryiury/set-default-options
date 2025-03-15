bbd9add5bf531f20089f73b132809522db5ccfaf
Ruggery Iury Corrêa <ruggeryiury@gmail.com>
2025-03-15 06:15:11 -0300

    Better object key iteration logic

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,14 @@ import type { RequiredDeep } from 'type-fest'
  * Iterates the provided `object` to append the values from this key, and return the files appended.
  * - - - -
  * @param {Record<string, any>} object The object with values to be iterable and appended to the return object.
+ * @param {Record<string, any>} defaultValues The default values of the object.
  * @returns {Record<string, any>}
  */
-export const iterateObjKeysAndAppendValues = (object: Record<string, any>): Record<string, any> => {
-  const newO: Record<string, any> = {}
+export const iterateObjKeysAndAppendValues = (object: Record<string, any>, defaultValues: Record<string, any>): Record<string, any> => {
+  const newO: Record<string, any> = { ...defaultValues }
 
   for (const key of Object.keys(object)) {
-    if (typeof object[key] === 'object') newO[key] = { ...object[key], ...iterateObjKeysAndAppendValues(object[key]) }
+    if (typeof object[key] === 'object') newO[key] = iterateObjKeysAndAppendValues(object[key], defaultValues[key])
     else newO[key] = object[key]
   }
 
@@ -26,5 +27,5 @@ export const iterateObjKeysAndAppendValues = (object: Record<string, any>): Reco
  */
 export const setDefaultOptions = <T>(defaultOptions: RequiredDeep<T>, userOptions?: Partial<T>): RequiredDeep<T> => {
   if (!userOptions) return defaultOptions
-  return { ...(defaultOptions as Record<string, any>), ...iterateObjKeysAndAppendValues(userOptions) } as RequiredDeep<T>
+  return iterateObjKeysAndAppendValues(userOptions, defaultOptions as Record<string, any>) as RequiredDeep<T>
 }
